2202d4268ff4352f03dceddce340fce6c972434a
gurar1986 <gurar1986@gmail.com>
2022-08-21 16:35:11 -0400

    testlocal1

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println(findUniq(new double[]{ 1, 222, 1, 1, 1, 1 }));
+        System.out.println(findUniq(new double[]{ 1, 777, 1, 1, 1, 1 }));
 
     }
     /*
